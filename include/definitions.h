a98867b49f7c1612ae3814dfe5e7508ba7f660fb
nicholashanson <nicholashanson27@outlook.com>
2024-11-05 08:21:51 +0800

    Update definitions.h

--- include/definitions.h
+++ include/definitions.h
@@ -1,19 +1,16 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
-#include <tuple>
+//#define TEST
 #include <array>
 #include "settings.h"
 #include <memory>
 #include <vector>
 #include <algorithm>
 #include <cstdlib>
-#include <set>
 #include <numeric>
 #include <list>
 #include <random>
 #include <ranges>
-#include <chrono>
-#include <iostream>
 #include <iterator>
 #include <cassert>
 #include <map>
@@ -43,25 +40,28 @@ typedef enum
     ACTIVE
 } status_t;
 
-
-typedef std::pair<int,int> xy_t;
+// scalar types
 typedef int x_t;
 typedef int y_t;
-typedef std::array<int,settings::board_width+1> floor_t;
-typedef std::vector<int>::iterator top_gem_it;
+typedef int span_t;
+typedef int count_t;
+typedef int score_t;
+typedef bool check_t;
+
+// iterators
+typedef Gem* gem_raw_ptr;
+typedef GemStack* gemstack_raw_ptr;
 typedef std::unique_ptr<Gem> gem_uptr;
+typedef std::vector<int>::iterator top_gem_it;
+
+// aggregate types
+typedef std::pair<int,int> xy_t;
 typedef std::list<gem_uptr> connector_t;
 typedef std::vector<connector_t> blob_t;
-typedef std::array<gem_uptr, 3> gemstack_elements_t;
-typedef int count_t;
-typedef bool check_t;
-typedef int span_t;
-typedef Gem* gem_raw_ptr;
-typedef std::array<color_t, 3> gemstack_colors_t;
-typedef GemStack* gemstack_raw_ptr;
 typedef std::vector<gem_raw_ptr> column_t;
+typedef std::array<color_t, 3> gemstack_colors_t;
+typedef std::array<gem_uptr, 3> gemstack_elements_t;
+typedef std::array<int,settings::board_width+1> floor_t;
 typedef std::array<column_t, settings::board_width + 1> grid_t;
-typedef int score_t;
 
 #endif // DEFINITIONS_H
-
