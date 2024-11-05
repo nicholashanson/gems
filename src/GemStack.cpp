4a9f5445e3205df504471fd54dfca58db1240179
nicholashanson <nicholashanson27@outlook.com>
2024-11-05 08:16:58 +0800

    Update GemStack.cpp

--- src/GemStack.cpp
+++ src/GemStack.cpp
@@ -5,30 +5,60 @@
 GemStack::GemStack() : current_orientation(UP) {}
 GemStack::~GemStack(){}
 
-auto GemStack::get_first_x() -> x_t{return get_first_gem()->get_x();}
-auto GemStack::get_first_y() -> y_t{return get_first_gem()->get_y();}
-auto GemStack::get_middle_x() -> x_t{return get_middle_gem()->get_x();}
-auto GemStack::get_middle_y() -> y_t{return get_middle_gem()->get_y();}
-auto GemStack::get_last_x() -> x_t{return get_last_gem()->get_x();}
-auto GemStack::get_last_y() -> y_t{return get_last_gem()->get_y();}
-auto GemStack::get_first_xy() -> xy_t{return get_first_gem()->get_xy();}
-auto GemStack::get_middle_xy() -> xy_t{return get_middle_gem()->get_xy();}
-auto GemStack::get_last_xy() -> xy_t{return get_last_gem()->get_xy();}
+auto GemStack::get_first_x() -> x_t
+{return get_first_gem()->get_x();}
 
-auto GemStack::gemstack_length() const -> int{return gems.size();}
+auto GemStack::get_first_y() -> y_t
+{return get_first_gem()->get_y();}
 
-auto GemStack::get_current_orientation() const -> orientation_t{return current_orientation;}
+auto GemStack::get_middle_x() -> x_t
+{return get_middle_gem()->get_x();}
 
-auto GemStack::get_first_gem() -> gem_raw_ptr{return first(gems).get();}
-auto GemStack::get_middle_gem() -> gem_raw_ptr{return middle(gems).get();}
-auto GemStack::get_last_gem() -> gem_raw_ptr{return last(gems).get();}
+auto GemStack::get_middle_y() -> y_t
+{return get_middle_gem()->get_y();}
+
+auto GemStack::get_last_x() -> x_t
+{return get_last_gem()->get_x();}
+
+auto GemStack::get_last_y() -> y_t
+{return get_last_gem()->get_y();}
+
+auto GemStack::get_first_xy() -> xy_t
+{return get_first_gem()->get_xy();}
+
+auto GemStack::get_middle_xy() -> xy_t
+{return get_middle_gem()->get_xy();}
+
+auto GemStack::get_last_xy() -> xy_t
+{return get_last_gem()->get_xy();}
+
+auto GemStack::gemstack_length() const -> int
+{return gems.size();}
+
+auto GemStack::get_current_orientation() const -> orientation_t
+{return current_orientation;}
+
+auto GemStack::get_first_gem() -> gem_raw_ptr
+{return first(gems).get();}
+
+auto GemStack::get_middle_gem() -> gem_raw_ptr
+{return middle(gems).get();}
+
+auto GemStack::get_last_gem() -> gem_raw_ptr
+{return last(gems).get();}
 
 // setters
-auto GemStack::set_first_xy(const x_t x, const y_t y) -> void{this->get_first_gem()->set_xy(x,y);}
-auto GemStack::set_middle_xy(const x_t x, const y_t y) -> void{this->get_middle_gem()->set_xy(x,y);}
-auto GemStack::set_last_xy(const x_t x, const y_t y) -> void{this->get_last_gem()->set_xy(x,y);}
+auto GemStack::set_first_xy(const x_t x, const y_t y) -> void
+{this->get_first_gem()->set_xy(x,y);}
 
-auto GemStack::get_gems() -> gemstack_elements_t*{return &gems;}
+auto GemStack::set_middle_xy(const x_t x, const y_t y) -> void
+{this->get_middle_gem()->set_xy(x,y);}
+
+auto GemStack::set_last_xy(const x_t x, const y_t y) -> void
+{this->get_last_gem()->set_xy(x,y);}
+
+auto GemStack::get_gems() -> gemstack_elements_t*
+{return &gems;}
 
 auto GemStack::get_gem_colors() -> gemstack_colors_t
 {
@@ -40,8 +70,10 @@ auto GemStack::get_gem_colors() -> gemstack_colors_t
 
 auto GemStack::rotate_gemstack() -> void
 {
-    swap_colors( first(gems).get(), last(gems).get() );
-    swap_colors( last(gems).get(), middle(gems).get() );
+    swap_colors( first(gems).get(),
+                 middle(gems).get() );
+    swap_colors( last(gems).get(),
+                 middle(gems).get() );
 }
 
 auto GemStack::toggle_orientation() -> void
@@ -194,7 +226,3 @@ auto GemStack::populate_gemstack(gemstack_colors_t gem_colors) -> void
         settings::board_middle,
         settings::last_starting_y);
 }
-
-
-
-
